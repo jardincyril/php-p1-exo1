f6eefab009d554c908fd488a2472dfd7a19d718c
cyril jardin <cyilroc60@hotmail.fr>
2018-07-26 15:08:58 +0200

    php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>exo1</title>
+  </head>
+  <body>
+    <p>
+    <?php
+    $name = 'cyril';
+    echo $name;
+     ?>
+   </p>
+  </body>
+</html>
